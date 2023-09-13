3e2e4912732799f67edf1ab856636415163491e9
caressaputri <caressaputriyuliantoro@gmail.com>
2023-09-13 09:28:49 +0700

    update 2

--- main/models.py
+++ main/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='product_images/')  # Tambahkan bidang gambar
 
-class Item(models.Model):
-    name = models.CharField(max_length=100)
-    amount = models.IntegerField()
-    description = models.TextField()
+    def __str__(self):
+        return self.name
